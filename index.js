6250c855245066ba0550e6d8b3897f782296dfaf
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-07-06 07:22:09 +0200

    artist+player done

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 
 
+const playMusic = (card) => {
+  const audios = document.querySelector("audio")
+  audios.src = card.preview
+  audios.play()
+  playInfo(card)
+}
+
+const playInfo = (card) => {
+  let playerImg = document.querySelector(".card-img-bottom")
+  let playerTitle = document.querySelector(".h6-bottom")
+  let playerName = document.querySelector(".p-bottom")
+  playerImg.src = card.album.cover_medium
+  playerTitle.innerText = card.title_short
+  playerName.innerText = card.artist.name
+
+}
+
+
+
 let lastTypedInput = localStorage.getItem("input")
 let input = lastTypedInput ? lastTypedInput : "Sia";
 const userInput = (event) => {
  
   input = event.target.value.toLowerCase()
   localStorage.setItem("input", event.target.value)
-  console.log(input)
+  // console.log(input)
   if (event.key === "Enter"){
     fetchData()
     event.preventDefault()
@@ -20,17 +39,20 @@ const fetchData = (q = input) => {
   fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${q}`)
 .then(res => res.json())
 .then(response => {
-console.log(response);
+// console.log(response);
 
 let theRes = response.data;
-console.log(theRes)
+// console.log(theRes)
 
 let theRow = document.querySelector("#card-rows")
 theRow.innerHTML = ""
 theRes.forEach(card => {
-  // console.log("Here is the card", card)
-  theRow.innerHTML += `
-                <div class="col-sm-3 pl-0">
+  console.log("Here is the card", card)
+  let col = document.createElement("div")
+  col.classList.add("col-sm-3", "pl-0")
+  col.onclick = () => playMusic(card)
+  col.innerHTML += `
+                
                   <div class="card h-100">
                     <div class="position-relative">
                       <img
@@ -50,9 +72,9 @@ theRes.forEach(card => {
                     <a href="./artist.html?artistId=${card.artist.id}"><p class="card-text text-white-50">${card.artist.name}</p></a>
                     </div>
                   </div>
-                </div>
+               
   `
-  
+  theRow.appendChild(col)
 });
 
 
